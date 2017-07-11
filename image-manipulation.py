2f58e3734289aa599c7cb07100c6112f2fa1db37
GeekComputings <nasser.abboud88@gmail.com>
2017-07-11 19:17:38 +0300

    Add files via upload

---
+++ image-manipulation.py
@@ -0,0 +1,11 @@
+from PIL import ImageEnhance
+import os
+size_256 = (256,256)
+for f in os.listdir('.'):
+    if f.endswith('.jpg'):
+        i = Image.open(f).convert('RGB')
+        fn, fext = os.path.splitext(f)
+        enhancer = ImageEnhance.Brightness(i)
+        i = enhancer.enhance(1.5)
+        img = i.resize(size_256, Image.ANTIALIAS)
+        img.save('Clean_darth vader/{}_clean{}'.format(fn, fext))
